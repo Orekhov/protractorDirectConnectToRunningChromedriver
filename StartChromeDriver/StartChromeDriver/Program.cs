d99e0946ad042ab52a2f8c165e23ba3400e2447b
Artemy Orekhov <AOrekhov2@slb.com>
2018-03-18 16:04:11 +0100

    add code for starting chrome driver

---
+++ StartChromeDriver/StartChromeDriver/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace StartChromeDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ChromeDriverPath = @"ChromeDriver\chromedriver.exe";
+            var processStartInfo = new ProcessStartInfo(ChromeDriverPath, "--port=9515")
+            {
+                UseShellExecute = false,
+                LoadUserProfile = true
+            };
+            var theProcess = Process.Start(processStartInfo);
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
